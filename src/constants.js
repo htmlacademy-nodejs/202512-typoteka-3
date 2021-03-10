a43cc55280deabcdd835bc10c81c36b8458019d4
Panova Natalia <pannat.job@gmail.com>
2021-03-10 14:28:29 +0300

    12. Личный проект: сервер наносит первый удар

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,19 @@ const ExitCode = {
   exception: 1
 };
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401
+};
+const ORANGE = `#ffa500`;
+
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
-  ExitCode
+  ORANGE,
+  ExitCode,
+  HttpCode
 };
--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,11 +1,13 @@
 const help = require(`./help`);
 const version = require(`./version`);
 const generate = require(`./generate`);
+const server = require(`./server`);
 
 const Cli = {
   [help.name]: help,
   [generate.name]: generate,
-  [version.name]: version
+  [version.name]: version,
+  [server.name]: server
 };
 
 module.exports = {
