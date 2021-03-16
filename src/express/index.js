a07a03dbe4ab5cd313f563eee17685ee62ec2139
Panova Natalia <pannat.job@gmail.com>
2021-03-16 13:42:12 +0300

    16. Личный проект: шаблонизируй это

--- src/express/index.js
+++ src/express/index.js
@@ -1,11 +1,12 @@
 const express = require(`express`);
 const path = require(`path`);
+const chalk = require(`chalk`);
 
 const DEFAULT_PORT = 8080;
 const Dir = {
   PUBLIC: `public`,
   TEMPLATES: `templates`
-}
+};
 
 const mainRoutes = require(`./routes/main`);
 const myRoutes = require(`./routes/my`);
@@ -14,14 +15,14 @@ const articlesRoutes = require(`./routes/articles`);
 const app = express();
 
 app.set(`views`, path.resolve(__dirname, Dir.TEMPLATES));
-app.set(`view engine`, `pug`)
+app.set(`view engine`, `pug`);
 
 app.use(express.static(path.resolve(__dirname, Dir.PUBLIC)));
 app.use(`/`, mainRoutes);
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
-app.use(`*`, (req, res) => res.render(`pages/404`));
+app.use(`*`, (req, res) => res.render(`pages/404`, {isGuest: true}));
 
 app.listen(DEFAULT_PORT, () => {
-  console.log(`Сервер запущен на порту ${DEFAULT_PORT}`);
+  console.log(chalk.bgBlueBright.black(`Сервер запущен на порту ${DEFAULT_PORT}`));
 });
--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -1,9 +1,34 @@
 const {Router} = require(`express`);
 const articlesRouter = new Router();
+const {
+  posts,
+  CATEGORIES
+} = require(`../mocks`);
 
-articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
+articlesRouter.get(`/add`, (req, res) => res.render(`pages/new-post`,
+    {
+      isAdmin: true
+    })
+);
 articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRouter.get(`/category/:id`, (req, res) => res.render(`pages/articles-by-category`));
-articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+
+articlesRouter.get(`/category/:id`, (req, res) => {
+  const currentCategory = CATEGORIES.find((category) => category.name === req.params[`id`]);
+  res.render(`pages/articles-by-category`,
+      {
+        isUser: true,
+        posts,
+        categories: CATEGORIES,
+        currentCategory
+      });
+}
+);
+
+articlesRouter.get(`/:id`, (req, res) => res.render(`pages/post`,
+    {
+      isUser: true,
+      categories: CATEGORIES
+    })
+);
 
 module.exports = articlesRouter;
